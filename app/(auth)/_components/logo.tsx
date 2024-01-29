0b92cd05fd19bddbfa04d5ae4b2efe304988dde4
Joshua <joshuaarnaez22@gmail.com>
2024-01-29 20:37:19 +0800

    first commit

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gay-4">
+      <div className="bg-white p-4 rounded-full">
+        <Image src="/logo.svg" height={80} width={80} alt="stream-hub-logo" />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">Stream-Hub</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s play</p>
+      </div>
+    </div>
+  );
+};
