d669fd2664744e80f8d09c29ddbc8de09fcb0f36
Joshua <joshuaarnaez22@gmail.com>
2024-01-29 20:45:07 +0800

    add testing prisma client

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 export default async function Home() {
+  const users = await prisma.user.findMany({});
+  console.log(users);
   return (
     <div className="">
       <Button>Button</Button>
